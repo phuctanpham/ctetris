fa750c9e5a3de602e9e9c569d1c556e3ed2f29bd
vingo1301 <vingo1301@gmail.com>
2026-04-15 14:30:46 +0700

    25730160/ them logic game over

--- app/src/gameCore/TetrisGame/Tetris/main.cpp
+++ app/src/gameCore/TetrisGame/Tetris/main.cpp
@@ -26,7 +26,7 @@ int main()
 {
     srand((unsigned int)time(0));
     // khởi tạo window
-    sf::RenderWindow window(sf::VideoMode({ 800, 720 }), "Hoc SFML - Buoc 6: Game Score");
+    sf::RenderWindow window(sf::VideoMode({ 800, 720 }), "Hoc SFML - Buoc 7: Game Over");
     float offsetX = (800 - N * BLOCK_SIZE) / 2.0f;
     float offsetY = (720 - M * BLOCK_SIZE) / 2.0f + 20.0f;
     // Bảng 7 màu sắc cho 7 khối (Thêm màu đen ở index 0)
@@ -52,10 +52,17 @@ int main()
 
     // ================= TẢI FONT CHỮ VÀ SETUP TEXT =================
     sf::Font font;
-    // Bắt buộc phải có file arial.ttf trong thư mục chứa code
+    // Bắt buộc phải có file arial_bold.ttf trong thư mục chứa code
     if (!font.openFromFile("arial_bold.ttf")) {
-        // Nếu chạy lên mà không thấy chữ, nghĩa là SFML không tìm thấy file font này
+        //
     }
+    // Chữ Game OVer
+    sf::Text gameOverText(font);
+    gameOverText.setString("GAME OVER\nPress R to Restart");
+    gameOverText.setCharacterSize(35);
+    gameOverText.setFillColor(sf::Color::Red);
+    // Căn ra giữa màn hình
+    gameOverText.setPosition({ offsetX - 30.f, offsetY + 200.f });
     // Chữ SCORE bên trái
     sf::Text scoreText(font);
     scoreText.setCharacterSize(28);
@@ -79,6 +86,7 @@ int main()
     int dx = 0;
     bool rotate = false;
     float timer = 0, delay = 0.3f;
+    bool isGameOver = false;
     sf::Clock clock;
 
     // Hàm Lambda (Hàm ẩn) để sinh khối mới cho code gọn gàng
@@ -88,6 +96,9 @@ int main()
         for (int i = 0; i < 4; i++) {
             a[i].x = figures[n][i] % 2 + 4;
             a[i].y = figures[n][i] / 2;
+        }
+        if (!check()) {
+            isGameOver = true;
         }
 
         // Random khối mới cho lượt sau
@@ -106,79 +117,95 @@ int main()
             if (event->is<sf::Event::Closed>()) window.close();
 
             if (const auto* keyEvent = event->getIf<sf::Event::KeyPressed>()) {
-                if (keyEvent->scancode == sf::Keyboard::Scancode::Up || keyEvent->scancode == sf::Keyboard::Scancode::W)
-                    rotate = true;
-                else if (keyEvent->scancode == sf::Keyboard::Scancode::Left || keyEvent->scancode == sf::Keyboard::Scancode::A)
-                    dx = -1;
-                else if (keyEvent->scancode == sf::Keyboard::Scancode::Right || keyEvent->scancode == sf::Keyboard::Scancode::D)
-                    dx = 1;
+                // CHO PHÉP NHẤN R ĐỂ RESTART KHI GAME OVER
+                if (isGameOver && keyEvent->scancode == sf::Keyboard::Scancode::R) {
+                    // Reset ma trận
+                    for (int i = 0; i < M; i++)
+                        for (int j = 0; j < N; j++)
+                            field[i][j] = 0;
+                    score = 0;
+                    isGameOver = false;
+                    spawnNewBlock();
+                }
+                if (!isGameOver) {
+                    if (keyEvent->scancode == sf::Keyboard::Scancode::Up || keyEvent->scancode == sf::Keyboard::Scancode::W)
+                        rotate = true;
+                    else if (keyEvent->scancode == sf::Keyboard::Scancode::Left || keyEvent->scancode == sf::Keyboard::Scancode::A)
+                        dx = -1;
+                    else if (keyEvent->scancode == sf::Keyboard::Scancode::Right || keyEvent->scancode == sf::Keyboard::Scancode::D)
+                        dx = 1;
+                }
+                
             }
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S)) delay = 0.05f;
         else delay = 0.3f;
 
-        // --- Di chuyển ngang ---
-        if (dx != 0) {
-            for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
-            if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
-            dx = 0;
-        }
-
-        // ================= THỰC THI PHÉP XOAY =================
-        if (rotate) {
-            Point p = a[1]; // Lấy mảnh thứ 2 làm tâm
-            for (int i = 0; i < 4; i++) {
-                b[i] = a[i]; // Sao lưu trước khi biến đổi
-
-                // Công thức nhân ma trận xoay
-                int deltaX = a[i].y - p.y;
-                int deltaY = a[i].x - p.x;
-
-                a[i].x = p.x - deltaX;
-                a[i].y = p.y + deltaY;
+        if (!isGameOver) {
+            // --- Di chuyển ngang ---
+            if (dx != 0) {
+                for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
+                if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+                dx = 0;
             }
-            // Nếu xoay xong mà đâm vào tường -> Hoàn tác
-            if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
 
-            rotate = false; // Tắt cờ
-        }
-
-        // --- Rơi xuống & Chốt gạch ---
-        if (timer > delay) {
-            for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
-
-            // NẾU CHẠM ĐÁY HOẶC GẠCH CŨ
-            if (!check()) {
-                // Chép tọa độ cũ (b) vào mảng field, lưu lại mã màu
+            // ================= THỰC THI PHÉP XOAY =================
+            if (rotate) {
+                Point p = a[1]; // Lấy mảnh thứ 2 làm tâm
                 for (int i = 0; i < 4; i++) {
-                    field[b[i].y][b[i].x] = colorNum;
-                }
-                // Sinh khối mới
-                spawnNewBlock();
-            }
-            timer = 0;
-        }
-        // --- Thuật toán Xóa hàng (Line Clearing) ---
-        int linesCleared = 0; // Biến đếm số hàng vừa ăn được
-        int k = M - 1; // k trỏ vào hàng dưới cùng
-        for (int i = M - 1; i > 0; i--) {
-            int count = 0; // Đếm số khối gạch trong hàng i
-            for (int j = 0; j < N; j++) {
-                if (field[i][j]) count++;
-                field[k][j] = field[i][j]; // Chép đè dữ liệu từ hàng i xuống hàng k
-            }
-            // Nếu hàng i chưa đầy (count < 10), ta cho phép hàng k dịch lên trên
-            // Nếu hàng i bị đầy, k đứng im để vòng lặp sau chép hàng phía trên đè xuống nó
-            if (count < N) {
-                k--;
-            } else {
-                linesCleared++; // Đánh dấu đã xóa 1 hàng
-            };
-        }
+                    b[i] = a[i]; // Sao lưu trước khi biến đổi
 
-        if (linesCleared >= 1) {
-            score += linesCleared * 100;
+                    // Công thức nhân ma trận xoay
+                    int deltaX = a[i].y - p.y;
+                    int deltaY = a[i].x - p.x;
+
+                    a[i].x = p.x - deltaX;
+                    a[i].y = p.y + deltaY;
+                }
+                // Nếu xoay xong mà đâm vào tường -> Hoàn tác
+                if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+
+                rotate = false; // Tắt cờ
+            }
+
+            // --- Rơi xuống & Chốt gạch ---
+            if (timer > delay) {
+                for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
+
+                // NẾU CHẠM ĐÁY HOẶC GẠCH CŨ
+                if (!check()) {
+                    // Chép tọa độ cũ (b) vào mảng field, lưu lại mã màu
+                    for (int i = 0; i < 4; i++) {
+                        field[b[i].y][b[i].x] = colorNum;
+                    }
+                    // Sinh khối mới
+                    spawnNewBlock();
+                }
+                timer = 0;
+            }
+            // --- Thuật toán Xóa hàng (Line Clearing) ---
+            int linesCleared = 0; // Biến đếm số hàng vừa ăn được
+            int k = M - 1; // k trỏ vào hàng dưới cùng
+            for (int i = M - 1; i > 0; i--) {
+                int count = 0; // Đếm số khối gạch trong hàng i
+                for (int j = 0; j < N; j++) {
+                    if (field[i][j]) count++;
+                    field[k][j] = field[i][j]; // Chép đè dữ liệu từ hàng i xuống hàng k
+                }
+                // Nếu hàng i chưa đầy (count < 10), ta cho phép hàng k dịch lên trên
+                // Nếu hàng i bị đầy, k đứng im để vòng lặp sau chép hàng phía trên đè xuống nó
+                if (count < N) {
+                    k--;
+                }
+                else {
+                    linesCleared++; // Đánh dấu đã xóa 1 hàng
+                };
+            }
+
+            if (linesCleared >= 1) {
+                score += linesCleared * 100;
+            }
         }
 
         scoreText.setString("SCORE\n" + std::to_string(score));
@@ -223,12 +250,17 @@ int main()
             }
         }
 
-        for (int i = 0; i < 4; i++) {
-            float pixelX = offsetX + a[i].x * BLOCK_SIZE;
-            float pixelY = offsetY + a[i].y * BLOCK_SIZE;
-            blockCell.setFillColor(colors[colorNum]);
-            blockCell.setPosition({ pixelX, pixelY });
-            window.draw(blockCell);
+        if (!isGameOver) {
+            for (int i = 0; i < 4; i++) {
+                blockCell.setFillColor(colors[colorNum]);
+                blockCell.setPosition({ offsetX + a[i].x * BLOCK_SIZE, offsetY + a[i].y * BLOCK_SIZE });
+                window.draw(blockCell);
+            }
+        }
+
+        // VẼ CHỮ GAME OVER LÊN LỚP TRÊN CÙNG
+        if (isGameOver) {
+            window.draw(gameOverText);
         }
 
         window.display();
