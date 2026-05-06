9f9e1cc443d014aa0697d042101907b3e3552d65
phucpt <phucpt+g1tla***calls.co-h-7>
2026-05-06 20:40:08 +0700

    phucpt: xoa ham remove line

--- main.cpp
+++ main.cpp
@@ -108,21 +108,6 @@ void draw(){
     for (int i = 0 ; i < H ; i++, cout<<endl)
         for (int j = 0 ; j < W ; j++) cout<<board[i][j];
 }
-void removeLine(){
-    int i,j;
-    for (i = H-2 ; i > 0 ; i-- ){
-        for (j = 0 ; j < W ; j++)
-            if (board[i][j] == ' ') break;
-        if (j == W){
-            for (int ii = i ; ii > 0 ; ii--)
-                for (int jj = 0; jj < W; jj++)
-                    board[ii][jj] = board[ii-1][jj];
-            i++;
-            draw();
-            _sleep(200);
-        }
-    }
-}
 
 int main()
 {
@@ -141,7 +126,6 @@ int main()
         if (canMove(0,1)) y++;
         else{
             block2Board();
-            removeLine();
             x = 5; y = 0; b = rand()%7;
         }
         block2Board();
