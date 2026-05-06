a7bbdbb8fad06ee9d9b7a1c28a3638929c972a5d
25730118 Nguyễn Trung Kiên <nguyenkien246135@gmail.com>
2026-05-06 14:31:23 +0000

    kiennt:Viết code chỗ xoay block

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-#include <conio.h>
+#include <ctime>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/select.h>
 
 using namespace std;
 #define H 20
@@ -7,6 +11,10 @@ using namespace std;
 char board[H][W] = {};
 
 int x, y, b;
+int blockType, rotationIndex;
+const int rotationStart[] = {0, 2, 3, 7, 9, 11, 15};
+const int rotationCount[] = {2, 1, 4, 2, 2, 4, 4};
+struct termios oldTermios;
 char blocks[][4][4] ={
     // I - 2 rotations
     {{' ','I',' ',' '},
@@ -109,6 +117,23 @@ bool canMove(int dx, int dy){
             }
     return true;
 }
+bool canRotate(){
+    int nextRotation = (rotationIndex + 1) % rotationCount[blockType];
+    int nextBlock = rotationStart[blockType] + nextRotation;
+    for (int i = 0; i < 4; i++ )
+        for (int j = 0; j < 4; j++ )
+            if (blocks[nextBlock][i][j] != ' ') {
+                int xt = x + j;
+                int yt = y + i;
+                if (xt < 1 || xt >= W-1 || yt >= H-1 ) return false;
+                if (board[yt][xt] != ' ') return false;
+            }
+    return true;
+}
+void rotateBlock(){
+    rotationIndex = (rotationIndex + 1) % rotationCount[blockType];
+    b = rotationStart[blockType] + rotationIndex;
+}
 void block2Board(){
     for (int i = 0; i < 4; i++ )
         for (int j = 0; j < 4; j++ )
@@ -150,7 +175,7 @@ void removeLine(){
     }
 }
 void draw(){
-    system("cls");
+    system("clear");
 
     for (int i = 0 ; i < H ; i++, cout<<endl)
         for (int j = 0 ; j < W ; j++) {
@@ -159,10 +184,44 @@ void draw(){
         }
 }
 
+void initTerminal(){
+    tcgetattr(STDIN_FILENO, &oldTermios);
+    struct termios newTermios = oldTermios;
+    newTermios.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newTermios);
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+}
+
+void resetTerminal(){
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldTermios);
+}
+
+bool kbhit(){
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    struct timeval tv = {0, 0};
+    return select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv) > 0;
+}
+
+char getch(){
+    char c = 0;
+    read(STDIN_FILENO, &c, 1);
+    return c;
+}
+
+void sleepMs(int ms){
+    usleep(ms * 1000);
+}
+
 int main()
 {
     srand(time(0));
-    x = 5; y = 0; b = rand()%19;
+    initTerminal();
+    blockType = rand()%7;
+    rotationIndex = rand()%rotationCount[blockType];
+    b = rotationStart[blockType] + rotationIndex;
+    x = 5; y = 0;
     initBoard();
     while (1){
         boardDelBlock();
@@ -170,17 +229,22 @@ int main()
             char c = getch();
             if (c == 'a' && canMove(-1,0)) x--;
             if (c == 'd' && canMove( 1,0)) x++;
+            if (c == 'w' && canRotate()) rotateBlock();
             if (c == 'x' && canMove( 0,1)) y++;
             if (c == 'q') break;
         }
         if (canMove(0,1)) y++;
         else{
             block2Board();
-            x = 5; y = 0; b = rand()%19;
+            blockType = rand()%7;
+            rotationIndex = rand()%rotationCount[blockType];
+            b = rotationStart[blockType] + rotationIndex;
+            x = 5; y = 0;
         }
         block2Board();
         draw();
-        _sleep(500);
+        sleepMs(500);
     }
+    resetTerminal();
     return 0;
 }
